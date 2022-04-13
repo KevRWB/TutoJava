bca9c12c857f580fa11dc93ab4b7e9a66e3c4c2c
KevRWB <kevinrenaultoff@gmail.com>
2022-04-13 22:51:42 +0200

    Creating javaTest File

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Comment ça va ?");
+
+        String name = "Roger";
+        int age = 54;
+
+
+
+    }
+}
