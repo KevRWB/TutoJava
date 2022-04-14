5fbf85cb98e70ef2159452604644111227c8fc95
KevRWB <kevinrenaultoff@gmail.com>
2022-04-15 00:29:52 +0200

    New feature "warning balance"

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -23,22 +23,42 @@ public class BankAccount {
 
     public void withdraw (double withdrawAmount) {
         double withdraw = withdrawAmount;
-        accountBalance -= withdrawAmount;
-        System.out.println("Hello " + accountOwner +", you have withdraw " + withdraw + "$ on your account numero " + accountNumber +". You're new balance is " + accountBalance + "$") ;
+
+        if (withdrawAmount > accountBalance) {
+            System.out.println("You don't have enough founds too withdraw this amount.");
+
+        } else {
+            accountBalance -= withdrawAmount;
+            System.out.println("Hello " + accountOwner +", you have withdraw " + withdraw + "$ on your account numero " + accountNumber +". You're new balance is " + accountBalance + "$") ;
+        }
+
     }
+
+    public void warningBalance () {
+        if (accountBalance <= 0) {
+            System.out.println("Warning" + accountOwner + ", you're account balance is 0 or less, please refund as soon as possible.");
+        }
+    }
+
+
 
     //main method
     public static void main (String [] args){
 
         //create new objects of BankAccount Class
-        BankAccount rogerAccount = new BankAccount("Roger", 12345, 1000);
+        BankAccount rogerAccount = new BankAccount("Roger", 12345, 0);
         BankAccount nadineAccount = new BankAccount("Nadine", 54321, 2000);
 
+        //print new account status
         System.out.println(rogerAccount);
         System.out.println(nadineAccount);
 
+        rogerAccount.warningBalance();
+        nadineAccount.warningBalance();
+
+        //call methods deposit & withdraw
         rogerAccount.deposit(80);
-        nadineAccount.withdraw(170);
+        nadineAccount.withdraw(2500);
     }
 
     public String toString(){
