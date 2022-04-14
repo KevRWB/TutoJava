07f403806610e2a939a0c44a82347bc96709fd68
KevRWB <kevinrenaultoff@gmail.com>
2022-04-14 10:15:17 +0200

    Change files to create Car Class

---
+++ src/Car.java
@@ -0,0 +1,21 @@
+public class Car {
+    //create car constructor
+
+    String carName;
+    String carColor;
+    int carYear;
+    boolean carIsRunning;
+    public Car(String name, String color, int year, boolean isRunning){
+        System.out.println("A new car is created");
+        carName = name;
+        carColor = color;
+        carYear = year;
+        carIsRunning = isRunning;
+    }
+
+    public static void main (String[] args){
+        Car Fiesta = new Car("Fiesta", "white", 2008, true);
+
+        System.out.println("This car is a " + Fiesta.carYear + " from " + Fiesta.carYear + ". His color is " + Fiesta.carColor);
+    }
+}
