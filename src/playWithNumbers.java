a3545d22ec1d0b023a831d69c16d02451530c5f6
KevRWB <kevinrenaultoff@gmail.com>
2022-04-21 16:13:42 +0200

    Complete PrimeDirective Class

--- src/playWithNumbers.java
+++ src/playWithNumbers.java
@@ -1,30 +1,49 @@
-import java.util.Arrays;
+// Import statement:
 import java.util.ArrayList;
-import java.util.Random;
-public class playWithNumbers {
 
+class PrimeDirective {
 
+    //constructor
+    public PrimeDirective(){
+    }
 
-    //main method
-    public static void main(String[] args){
-        ArrayList<Integer> numbersList = new ArrayList<Integer>();
-        int numberListSize = numbersList.size();
-
-        System.out.println(numberListSize);
-
-        if (args.length != 0){
-            for (String arg : args){
-                String str = arg;
-                int num = Integer.parseInt(str);
-
-                numbersList.add(num);
-
-                System.out.println(numbersList);
+    // isPrime ? method
+    public boolean isPrime(int number){
+        if(number == 2){
+            return true;
+        } else if(number < 2){
+            return false;
+        } else {
+            for(int i = 2; i < number; i++){
+                if(number % i == 0){
+                    return false;
+                }
             }
-        }else {
-            System.out.println("No Argument");
         }
+        return true;
+    }
+
+    //onlyPrime Method
+    public ArrayList<Integer> onlyPrimes(int[] numbers){
+        ArrayList<Integer> primes = new ArrayList<>();
+        for(int number : numbers){
+            if(isPrime(number)){
+                primes.add(number);
+            }
+        }
+        return primes;
+    }
+
+    //Main Method
+    public static void main(String[] args){
+        //Create new Instance of PrimeDirective
+        PrimeDirective pd = new PrimeDirective();
+
+        //Create a list of numbers to check
+        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+        System.out.println(pd.onlyPrimes(numbers));
+
     }
 
 }
-
