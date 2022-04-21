6abd1aa0e6bc137f9add125110440ab8b1e03f32
KevRWB <kevinrenaultoff@gmail.com>
2022-04-21 16:14:31 +0200

    Complete PrimeDirective Class

--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -1,15 +1,20 @@
 // Import statement:
-import java.util.Arrays;
+import java.util.ArrayList;
+
 class PrimeDirective {
 
-    // Add your methods here:
+    //constructor
+    public PrimeDirective(){
+    }
+
+    // isPrime ? method
     public boolean isPrime(int number){
         if(number == 2){
             return true;
         } else if(number < 2){
             return false;
         } else {
-            for(int i = 3; i < number; i++){
+            for(int i = 2; i < number; i++){
                 if(number % i == 0){
                     return false;
                 }
@@ -18,12 +23,26 @@ class PrimeDirective {
         return true;
     }
 
-    public static void main(String[] args) {
+    //onlyPrime Method
+    public ArrayList<Integer> onlyPrimes(int[] numbers){
+        ArrayList<Integer> primes = new ArrayList<>();
+        for(int number : numbers){
+            if(isPrime(number)){
+                primes.add(number);
+            }
+        }
+        return primes;
+    }
 
+    //Main Method
+    public static void main(String[] args){
+        //Create new Instance of PrimeDirective
         PrimeDirective pd = new PrimeDirective();
+
+        //Create a list of numbers to check
         int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
 
-        System.out.println(pd.isPrime(12));
+        System.out.println(pd.onlyPrimes(numbers));
 
     }
 
